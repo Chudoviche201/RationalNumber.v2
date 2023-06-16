885b7537cb687e3dd83a2dde53b40d5fe087a824
Vlad Hat <moki.hat@mail.ru>
2023-06-16 23:56:29 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumber a = new RationalNumber(1, 2);
+            RationalNumber b = new RationalNumber(3, 4);
+
+            RationalNumber sum = a + b;
+            RationalNumber difference = a - b;
+            RationalNumber product = a * b;
+            RationalNumber quotient = a / b;
+
+            Console.WriteLine($"Сумма: {sum}");
+            Console.WriteLine($"Разность: {difference}");
+            Console.WriteLine($"Произведение: {product}");
+            Console.WriteLine($"Частное: {quotient}");
+
+            Console.WriteLine($"a == b? {a == b}");
+            Console.WriteLine($"a != b? {a != b}");
+            Console.WriteLine($"a < b? {a < b}");
+            Console.WriteLine($"a <= b? {a <= b}");
+            Console.WriteLine($"a > b? {a > b}");
+            Console.WriteLine($"a >= b? {a >= b}");
+
+            Console.ReadLine();
+        }
+    }
+}
